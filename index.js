befac7d2984d9176a11e6dffbd5c961d2c2d6ce8
Leonfen <michelegunterland@gmail.com>
2022-01-01 11:30:13 +0100

    created a small project for who doesn't want to wish a happy new year to all of his friends

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const { Client } = require('whatsapp-web.js');
+const QRCode = require('qrcode')
+const opn = require('opn');
+const { response } = require('express');
+
+const client = new Client();
+
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    QRCode.toDataURL(qr, function (err, url) {
+        console.log('Scannerizza il tuo codice qr')
+        opn(url, { app: process.env.BROWSER });
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body.toLocaleLowerCase().includes('auguri')) {
+        msg.getChat().then((response) => {
+
+            if ((!response.name.toLocaleLowerCase().includes('med')) && (!response.name.toLocaleLowerCase().includes('uab')) && (!response.name.toLocaleLowerCase().includes('informatica'))) {
+                msg.reply('Auguri anche a voi e a famiglia! <3');
+            }
+
+        }).catch(err => console.log(err))
+    }
+
+    else if (msg.body.toLocaleLowerCase().includes('buon anno')) {
+        msg.getChat().then((response) => {
+
+            if ((!response.name.toLocaleLowerCase().includes('med')) && (!response.name.toLocaleLowerCase().includes('uab')) && (!response.name.toLocaleLowerCase().includes('informatica'))) {
+                msg.reply('Auguri anche a voi e a famiglia! <3');
+            }
+
+        }).catch(err => console.log(err))
+    }
+
+    else if (msg.body.toLocaleLowerCase().includes('buon compleanno')) {
+        msg.getChat().then((response) => {
+
+            if ((!response.name.toLocaleLowerCase().includes('med')) && (!response.name.toLocaleLowerCase().includes('uab')) && (!response.name.toLocaleLowerCase().includes('informatica'))) {
+                msg.reply('Buon compleanno anche a voi e a famiglia! <3');
+            }
+
+        }).catch(err => console.log(err))
+    }
+
+});
+
+client.initialize();
